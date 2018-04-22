e8035473c326240003a4dbcc1c80bfb0385b720c
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-22 18:22:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,41 +12,17 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value)
   return obj;
 }
 
-
 function deleteFromObjectByKey(obj, key)
 {
   var newObj =  Object.assign({},obj);
-  delete newObj
+  delete newObj[key];
+  return newObj;
 }
 
-  describe('deleteFromObjectByKey(object, key)', function() {
-    it('deletes `key` from a clone of object and returns the new object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-      var newObj = deleteFromObjectByKey(obj, 'prop')
+function destructivelyDeleteFromObjectByKey(obj, key)
+{
+  obj.dete[key];
+  return obj;
+}
 
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('does not modify the original object (it is non-destructive)', function() {
-      var obj = { prop: 1 }
-
-      deleteFromObjectByKey(obj, 'prop')
-      expect(obj['prop']).toBe(1)
-    })
-  })
-
-  describe('destructivelyDeleteFromObjectByKey(object, key)', function() {
-    it('returns object without the delete key/value pair', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(newObj['prop']).toBe(undefined)
-    })
-
-    it('modifies the original object', function() {
-      var obj = { prop: 1 }
-      var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
-
-      expect(obj['prop']).toBe(undefined)
-    })
-  })
+ 
